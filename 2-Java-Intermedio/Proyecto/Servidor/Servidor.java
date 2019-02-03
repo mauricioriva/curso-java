9664b8a47d5982fa884bfa235ddcd0959dc588ed
mauricioriva <mauricioriva@ciencias.unam.mx>
2019-02-02 19:52:30 -0600

    Proyecto 2

--- 2-Java-Intermedio/Proyecto/Servidor/Servidor.java
+++ 2-Java-Intermedio/Proyecto/Servidor/Servidor.java
@@ -12,29 +12,11 @@ public class Servidor{
         try {
             listener = new ServerSocket(9898);
             while (true) {
-                int i = 5;
-                listener.accept()
+                listener.accept();
             }
 
-        } catch (IOException ioe) {
-            System.out.println(ioe.getMessage());
-        }
-
-    }
-
-    private static class Archivo extends Thread{
-
-        private Socket socket;
-        private int nCliente;
-
-        public Archivo(Socket socket, int nCliente){
-            this.socket = socket;
-            this.nCliente = nCliente;
-            System.out.println("Nuevo cliente #" + nCliente + ", conexion en " + socket.getInetAddress());
-        }
-
-        public void run(){
-            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
 
     }
